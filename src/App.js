55a0e29761f9f50bd376a2ee98ef2bbd922b17d9
rajonhossain <rajonhossaindhaka@gmail.com>
2021-03-04 16:29:55 +0600

    fisr animation icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+import React, { useEffect } from "react";
+
 import logo from './logo.svg';
 import './App.css';
+import 'aos';
+import AOS from 'aos';
+import { FaBeer } from 'react-icons/fa';
+
+import { AiTwotonePhone, 
+  AiFillAudio, 
+  AiFillFolderOpen,
+  AiFillWindows,
+   } from "react-icons/ai";
+
+
+import { FcHome,
+FcNightPortrait,
+FcFilm,
+ } from "react-icons/fc";
+
+
+ 
+
 
 function App() {
+
+  useEffect(() => {
+   AOS.init({
+  duration: 1000,
+})
+
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+      
+      <div class="item" data-aos="fade-up"><FaBeer className="aa"/></div>
+
+      <div class="item" data-aos="fade-down"><AiFillFolderOpen className="aa"/></div>
+      <div class="item" data-aos="fade-right"><AiFillAudio className="aa"/></div>
+      <div class="item" data-aos="fade-left"><AiTwotonePhone className="aa"/></div>
+
+      <div class="item" data-aos="zoom-in"><AiFillWindows className="aa"/></div>
+      <div class="item" data-aos="zoom-out"><FcHome className="aa"/></div>
+      <div class="item" data-aos="zoom-out"><FcNightPortrait className="aa"/></div>      
+      <div class="dtd" data-aos="flip-down"><FcFilm className="aa"/></div>
+
+      <div class="item" data-aos="slide-up">7</div>
+
+      <div class="item" data-aos="flip-up">8</div>
+      <div class="item" data-aos="flip-down">9</div>
+      <div class="item" data-aos="flip-right">10</div>
+      <div class="item" data-aos="flip-left">11</div>
+</div>
   );
 }
 
